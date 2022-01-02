b860f6437b6c7ed785f75578522e0c90496bf8ec
andrey12v <andrey12v@gmail.com>
2022-01-01 21:47:04 -0600

    Use the function useEffect to prevent infinite loop when set local storage value
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599212#overview

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //useEffect helps toprevent infinite looop. 
+  //if we set isLoggedIn to 1 in the local storage and reload the site this local storage will always be 1 
+  //as a result of it we remove 1 from tehlocal storage in the logout function 
+  useEffect(() => {
+    
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if(storedUserLoggedInInformation === '1')
+    {
+      setIsLoggedIn(true);
+    }
+
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
